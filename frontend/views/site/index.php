ffcc029540ee3c2d43bc0a47d8e0c6180a5ceaf2
Kris <artas022@gmail.com>
2019-05-10 16:38:03 +0300

    first commit

---
+++ frontend/views/site/index.php
@@ -0,0 +1,26 @@
+<?php
+
+/* @var $this yii\web\View */
+
+$this->title = 'Основная страница';
+?>
+<div class="site-index">
+
+    <div class="jumbotron">
+        <h1>Pizza main page</h1>
+
+        <p class="lead">Добро пожаловать в ресторан!</p>
+
+        <p><a class="btn btn-lg btn-success" href="http://www.yiiframework.com">Функционала нет</a></p>
+    </div>
+
+    <div class="body-content">
+
+        <div class="row">
+            <h1 align="center">Ознакомьтесь с нашим меню:</h1>
+            <div class="col-lg-4">
+            </div>
+        </div>
+
+    </div>
+</div>
